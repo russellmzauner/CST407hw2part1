5ac19907c173f55edbab478f5838507c37e67eac
russellmzauner <russellmzauner@gmail.com>
2014-07-21 21:46:16 -0700

    Moderately cleaned and commented
    Couple questions in comments.

--- src/edu/oit/cst407/mypreference/OpinionActivity.java
+++ src/edu/oit/cst407/mypreference/OpinionActivity.java
@@ -1,56 +1,46 @@
 package edu.oit.cst407.mypreference;
 
 import android.app.Activity;
-import android.content.res.Configuration;
+
+// Didn't need this after all - why does this happen automatically?  
+// I thought I would need to check for the state change...
+
+//import android.content.res.Configuration;
+
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class OpinionActivity extends Activity{
-	
-	
-
-	
+		
 	@Override
 	public void onCreate(Bundle savedInstanceState){
-	
-		
+			
 		super.onCreate(savedInstanceState);
 		
 		setContentView(R.layout.activity_opinion);
 		
 		TextView orientationMessage = (TextView)findViewById(R.id.orientation_message);
 		
-		
-	if (this.getResources().getConfiguration().orientation == 1){
+		if (this.getResources().getConfiguration().orientation == 1) {
 			orientationMessage.setText(this.getString(R.string.orientation_portrait));
 			}
+		
 		else {
 			orientationMessage.setText(this.getString(R.string.orientation_landscape));
-			}
-		
-		
-		//setContentView(R.layout.activity_opinion);
-		
-		//TextView orientationMessage = (TextView)findViewById(R.id.orientation_message);
-		
-		// orientationMessage.setText(this.getString(R.string.orientation_text));
-		
-	}
-	
-	
+			}		
+		}
+
+	// I thought I would need this code to get it to detect orientation change.  Why not?
 	
 	/*
 	@Override
-	public void onConfigurationChanged(Configuration newConfig){
-		
-		
+	public void onConfigurationChanged(Configuration newConfig){		
 		
 		super.onConfigurationChanged(newConfig);
 		
 		setContentView(R.layout.activity_opinion);
 		
-		TextView orientationMessage = (TextView)findViewById(R.id.orientation_message);
-		
+		TextView orientationMessage = (TextView)findViewById(R.id.orientation_message);		
 		
 		if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE){
 			orientationMessage.setText(this.getString(R.string.orientation_landscape));
